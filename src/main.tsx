b8cb2239c51a19c50eeadce4e62afc8d9113e824
Trendy World <lucas.scribe01@gmail.com>
2026-03-02 16:06:35 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './AeroPaceStudio.tsx' // Import de votre application principale
+import './index.css' // Import de Tailwind CSS (si vous avez créé ce fichier)
 
+// On cible la div "root" de l'index.html et on y rend l'application
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
